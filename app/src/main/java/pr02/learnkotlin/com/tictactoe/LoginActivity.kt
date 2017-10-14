3e3c962b9ab7f79c770c31357596d3858a9244b8
abdul-rehman-2050 <supermind2002@gmail.com>
2017-10-14 06:02:40 +0500

    LoginActivity Added

---
+++ app/src/main/java/pr02/learnkotlin/com/tictactoe/LoginActivity.kt
@@ -0,0 +1,33 @@
+package pr02.learnkotlin.com.tictactoe
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+
+    }
+
+
+    fun tryMeLogIn(view: View){
+
+
+
+    }
+
+    fun registerNewUser(view: View){
+
+    }
+
+
+}
